7a19fbc2b10e23d0fd00d9baa5274d93a991d2da
tomazleme <67647378+tomazleme@users.noreply.github.com>
2023-04-19 18:53:26 -0300

    Update script_new.js

--- script_new.js
+++ script_new.js
@@ -1,27 +1,48 @@
 // Função para mostrar um passo específico do formulário e esconder os outros
 function showStep(stepId) {
-  // Seleciona todos os elementos que têm um ID começando com "step"
   const steps = document.querySelectorAll('[id^="step"]');
-
-  // Percorre cada passo e define sua propriedade 'display' como 'block' ou 'none', dependendo se é o passo ativo
   steps.forEach((step) => {
     step.style.display = step.id === stepId ? 'block' : 'none';
+  });
+}
+
+// Função para validar o endereço de email
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+// Função para lidar com o evento de clique ou pressionar Enter no Passo 1
+function handleStep1Submit() {
+  const emailInput = document.querySelector('#email-step1');
+  const email = emailInput.value;
+
+  if (isValidEmail(email)) {
+    showStep('step2');
+  } else {
+    alert('Por favor, insira um endereço de email válido.');
+  }
+}
+
+// Função para adicionar event listeners
+function addEventListeners() {
+  // Adiciona um event listener para o botão "Continuar" do Passo 1
+  document.querySelector('#step1 .btn-my-custom[aria-current="step2"]').addEventListener('click', handleStep1Submit);
+
+  // Adiciona um event listener ao campo de email para lidar com a tecla Enter
+  document.querySelector('#email-step1').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      handleStep1Submit();
+    }
   });
 }
 
 // Função para inicializar o estado do formulário, mostrando o Passo 1
 function initializeForm() {
   showStep('step1');
+  addEventListeners();
 }
 
-// Adiciona um event listener para o botão "Continuar" do Passo 1
-document.querySelector('#step1 .btn-my-custom[aria-current="step2"]').addEventListener('click', () => {
-  // Quando o botão "Continuar" é clicado, chama a função `showStep` com o ID do Passo 2
-  showStep('step2');
-});
-
 // Adiciona um event listener para quando a página for carregada
-document.addEventListener('DOMContentLoaded', () => {
-  // Quando a página é carregada, chama a função `initializeForm` para configurar o estado inicial do formulário
-  initializeForm();
-});
+document.addEventListener('DOMContentLoaded', initializeForm);
