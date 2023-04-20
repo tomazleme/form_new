a7f091e107914a14ba115e6d39d800daeeccc5ad
tomazleme <67647378+tomazleme@users.noreply.github.com>
2023-04-19 21:57:45 -0300

    Update script_new.js

--- script_new.js
+++ script_new.js
@@ -13,48 +13,94 @@ function isValidEmail(email) {
 }
 
 // Função para lidar com o evento de clique ou pressionar Enter no Passo 1
-function handleStep1Submit(event) {
-  event.preventDefault(); // Adicione esta linha para prevenir a ação padrão do botão
-
-  const emailInput = document.getElementById('email-step1');
+function handleStep1Submit() {
+  const emailInput = document.querySelector('#email-step1');
   const email = emailInput.value;
 
-  if (validateEmail(email)) {
+  if (isValidEmail(email)) {
     showStep('step2');
   } else {
-    emailInput.setCustomValidity('Por favor, insira um endereço de email válido.');
-    emailInput.reportValidity();
+    alert('Por favor, insira um endereço de email válido.');
   }
 }
 
-// Funçao para lidar com evento de clique no botão voltar do passo 2 para o passo 1
-function handleBackToStep1() {
+// Função para lidar com o evento de clique no botão "Voltar" do Passo 2
+function handleStep2Back() {
   showStep('step1');
 }
+// Função para lidar com o evento de clique no botão "Continuar" do Passo 2
+function handleStep2Submit() {
+  const select = document.querySelector('#step2 select');
+  const selectedOption = select.options[select.selectedIndex].value;
 
-function addEventListeners() {
-  const step1ContinueBtn = document.querySelector('#step1 .btn-my-custom[aria-current="step2"]');
-  step1ContinueBtn.addEventListener('click', handleStep1Submit);
-  step1ContinueBtn.addEventListener('keydown', handleStep1EnterPress);
-
-  const step2BackBtn = document.querySelector('#step2 .btn-my-custom[aria-current="back-step1"]');
-  step2BackBtn.addEventListener('click', handleBackToStep1);
+  if (selectedOption === 'a') {
+    showStep('step2A');
+  } else if (selectedOption === 'b') {
+    showStep('step2B');
+  }
 }
 
+// Função para lidar com o evento de clique no botão "Voltar" do Passo 2A
+function handleStep2ABack() {
+  showStep('step2');
+}
+
+// Função para lidar com o evento de clique no botão "Voltar" do Passo 2B
+function handleStep2BBack() {
+  showStep('step2');
+}
+
+// Função para lidar com o evento de clique no botão "Continuar" do Passo 2A
+function handleStep2ASubmit() {
+  const radioInputs = document.querySelectorAll('#radio-2A input[type="radio"]');
+  let selectedOption;
+
+  radioInputs.forEach((input) => {
+    if (input.checked) {
+      selectedOption = input.id;
+    }
+  });
+
+  if (selectedOption === '1p2A') {
+    showStep('step2A1');
+  } else if (selectedOption === '2p2A') {
+    showStep('step2A2');
+  } else if (selectedOption === '3p2A') {
+    showStep('step2A3');
+  }
+}
 
 // Função para adicionar event listeners
 function addEventListeners() {
   // Adiciona um event listener para o botão "Continuar" do Passo 1
   document.querySelector('#step1 .btn-my-custom[aria-current="step2"]').addEventListener('click', handleStep1Submit);
 
-  // Adiciona um event listener ao campo de email para lidar com a tecla Enter
+// Adiciona um event listener ao campo de email para lidar com a tecla Enter
   document.querySelector('#email-step1').addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
       event.preventDefault();
       handleStep1Submit();
     }
   });
+
+// Adiciona um event listener para o botão "Voltar" do Passo 2
+  document.querySelector('#step2 .btn-my-custom[aria-current="back-step1"]').addEventListener('click', handleStep2Back);
 }
+
+// Adiciona um event listener para o botão "Continuar" do Passo 2
+document.querySelector('#step2 .btn-my-custom[aria-current="step"]').addEventListener('click', handleStep2Submit);
+
+// Adiciona um event listener para o botão "Voltar" do Passo 2A
+document.querySelector('#step2A .btn-my-custom[aria-current="back-step2"]').addEventListener('click', handleStep2ABack);
+
+
+// Adiciona um event listener para o botão "Voltar" do Passo 2B
+document.querySelector('#step2B .btn-my-custom[aria-current="back-step2"]').addEventListener('click', handleStep2BBack);
+
+// Adiciona um event listener para o botão "Continuar" do Passo 2A
+document.querySelector('#step2A .btn-my-custom[aria-current="step"]').addEventListener('click', handleStep2ASubmit);
+
+
 
 // Função para inicializar o estado do formulário, mostrando o Passo 1
 function initializeForm() {
